599a42f3c8e3b35b2d3abc90ffe76d0c47880ae8
Ivan Kerin <ikerin@gmail.com>
2026-07-13 12:11:31 +0530

    fix(store): remove MaxOpenConns(1) restriction to eliminate nested query deadlock

--- internal/store/db.go
+++ internal/store/db.go
@@ -25,7 +25,6 @@ func Open(dsn string) (*sql.DB, error) {
 	if err != nil {
 		return nil, fmt.Errorf("open db: %w", err)
 	}
-	db.SetMaxOpenConns(1)
 	goose.SetBaseFS(migrations.FS)
 	goose.SetLogger(goose.NopLogger())
 	if err := goose.SetDialect("sqlite"); err != nil {
